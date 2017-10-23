ea5d247267d4935c6619790c9f59ce4586651ab4
Kfokasand <32450335+Kfokasand@users.noreply.github.com>
2017-10-23 10:07:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "simulation.hpp"
 #include <iostream>
 
-
 using namespace std;
 
 int main()
@@ -9,6 +8,7 @@ int main()
 	Simulation sim1(0.1);	
 	
 	sim1.Run();
+	//rerun to check for memory leaks
 	
 	
 	return 0;	
