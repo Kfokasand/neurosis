40a45d3c7607e6bc3d6c458bed97bf9f0f37a59e
Kfokasand <32450335+Kfokasand@users.noreply.github.com>
2017-11-05 12:16:55 +0100

    Delete CTestTestfile.cmake

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/INTRANET/fokasand/Desktop/myfiles/cpp/BA3/project/neurosis
-# Build directory: /home/INTRANET/fokasand/Desktop/myfiles/cpp/BA3/project/neurosis/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unittest "unittest")
-subdirs(googletest)
